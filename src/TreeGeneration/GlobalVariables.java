c0672431356351a788e2adac869fac1892c0f2e6
rwashahi <rimaturker@gmail.com>
2017-03-13 12:52:11 +0100

    Global Variables added

---
+++ src/TreeGeneration/GlobalVariables.java
@@ -0,0 +1,10 @@
+package TreeGeneration;
+
+import java.io.File;
+
+public class GlobalVariables 
+{
+	final static int levelOfTheTree=10;
+	final static String path_MainSkosFile= System.getProperty("user.dir")+ File.separator+ "skos_broader_CatCleaned_sort.txt";
+	static String pathMainCategories= System.getProperty("user.dir")+File.separator+"MainCategoryFile.txt";
+}
